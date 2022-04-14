726537ad25624242283168c119106e5b65160714
tzu-chien-hung <tina19960522@gmail.com>
2022-04-14 11:05:39 +0800

    Tina | fix aside js

--- js/aside.js
+++ js/aside.js
@@ -1,17 +1,40 @@
 const product = document.querySelectorAll('.section_all');
 const aside = document.querySelectorAll('.nav_right_bg_1');
-// console.log('aside');
+const nav_right_img = document.querySelectorAll('.nav_right_img');
+const svg = document.querySelectorAll('.svg_fill');
 
 document.addEventListener('scroll', function () {
-    console.log(document.documentElement.scrollTop);
+    // console.log(document.documentElement.scrollTop);  // 距離top的高度
     product.forEach(function (el, index) {
-        console.log(el.offsetTop);
-        if (Math.abs(el.offsetTop - document.documentElement.scrollTop)< 60) {
+        console.log(el.offsetTop);  // section 距離top的高度
+        if (Math.abs(el.offsetTop - document.documentElement.scrollTop) < 60 ) {
             aside.forEach(function (el, index) {
                 el.style = "border: 0px solid black";
             });
             aside[index].style = "display:block";
-            // console.log(aside);
-        }
-    })
-})
+                        svg.forEach(function (el, index) {
+                el.style = "";
+            });
+            svg[index].style = "fill: #016437";
+            nav_right_img.forEach(function (el, index) {
+                el.style = "";
+            });
+            nav_right_img[index].style = "background-color: #ffd02b";
+        };
+    });
+});
+
+// const product = document.querySelectorAll('.section_all');
+// const aside = document.querySelectorAll('.nav_right_bg_1');
+// console.log(product);
+// document.addEventListener('scroll', function () {
+//     product.forEach(function (el, index) {
+//         const height = el.offsetTop + (el.clientHeight/2)
+//         if (Math.abs(height - document.documentElement.scrollTop)< 50) {
+//             aside.forEach(function (el, index) {
+//                 el.style = "border: 0px solid black";
+//             });
+//             aside[index].style = "display:block";
+//         }
+//     })
+// });
